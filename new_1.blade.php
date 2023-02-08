3de0223a7a60e63f376b7680f3760292d0a8166b
anton_git <gorizont.k@mail.ru>
2023-02-08 21:56:21 +0700

    add new_1.blade.php

--- new_1.blade.php
+++ new_1.blade.php
@@ -1,37 +1,40 @@
 <!DOCTYPE html>
 <html lang="en">
 
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document_List</title>
-    </head>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document_List</title>
+</head>
 
-    <body>
-        <h1>Hello world!!!!</h1>
-        <p>
-            git init
-            git add .||file.php
-            git commit -m "text" //commit
-            git status //show
-            git log // show
-            git log --oneline
+<body>
+    <h1>Hello world!!!!</h1>
+    <p>
+        git init
+        git add .||file.php
+        git commit -m "text" //commit
+        git status //show
+        git log // show
+        git log --oneline
 
-            git revert id_commit // :wq
-            git reset id_commit //return
+        git revert id_commit // :wq
+        git reset id_commit //return
 
-            git checkout id_commit(number) || master //chechout in to branch
+        git checkout id_commit(number) || master //chechout in to branch
 
-            git branch name //create new
-            git branch -a //show all
+        git branch name //create new
+        git branch -a //show all
 
-            git merge name // in to master // a + b
+        git merge name // in to master // a + b
 
-            git remote add origin https://github.com/AntonKorob/s-see.git
-            git push -u origin master
+        git remote add origin https://github.com/AntonKorob/s-see.git
+        git push -u origin master
+        git clone https://github.com/AntonKorob/s-see.ru.git //cd htdocs
 
-        </p>
-    </body>
+        git pull //download all new file
+  
+    </p>
+</body>
 
 </html>
